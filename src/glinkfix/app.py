51bea92006a893c835a3c99d86abbdab2546c76e
Peter Nardi <pete@nardi.com>
2024-09-29 10:02:44 -0400

    Code linting.

--- src/glinkfix/app.py
+++ src/glinkfix/app.py
@@ -15,20 +15,24 @@ def main() -> None:
     1. Generate an argument parser to collect command line input.
     2. Call `fix_link` to perform link correction.
     """
-    msg = """This program takes a Google Drive sharing link for a
-    file and repackages it into a link that can be downloaded directly
-    (e.g. using curl) or embedded in a document to be viewed (e.g. an
-    image in a markdown document). Note: there is a size limit of 40MB
-    for a single file when using Google Drive links in this manner."""
+    msg = """
+    This program takes a Google Drive sharing link for a file and
+    repackages it into a link that can be downloaded directly (e.g.
+    using curl) or embedded in a document to be viewed (e.g. an image in
+    a markdown document). Note: there is a size limit of 40MB for a
+    single file when using Google Drive links in this manner.
+    """
 
     epi = f"Version: {VERSION}"
 
     parser = argparse.ArgumentParser(description=msg, epilog=epi)
 
-    msg = """ The default behavior for glinkfix is to repackage a Google
-    Drive link to make it suitable for embedding in a website. Use the
-    -d option if you want to repackage Google Drive link for direct
-    downloading instead (e.g. downloading using curl)."""
+    msg = """
+    The default behavior for glinkfix is to repackage a Google Drive
+    link to make it suitable for embedding in a website. Use the -d
+    option if you want to repackage Google Drive link for direct
+    downloading instead (e.g. downloading using curl).
+    """
     parser.add_argument(
         "-d",
         "--download",
